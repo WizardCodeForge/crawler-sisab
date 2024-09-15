ab0db5009fb5abc020d967db97607532d0539f00
gbcode98 <gabrielcarlossocial@gmail.com>
2024-09-15 03:42:20 -0300

    feat; add The driver.quit() is called in the finally block to ensure that the driver is closed even if an error occurs.

--- main.py
+++ main.py
@@ -2,24 +2,39 @@ from utils.setup_driver import setup_driver
 from scripts.equipes_homologadas import executar_equipes_homologadas
 from scripts.todas_equipes import executar_todas_equipes
 import os
+import time
 
 def main():
-    driver = setup_driver()
-
     # Caminho onde o CSV será salvo
     pasta_download = 'C:\\Users\\LKeuu\\OneDrive\\Documentos\\CSV\\'  # Usar scape com \\ para especificar caminho
     os.makedirs(pasta_download, exist_ok=True)
 
-    # Executar o primeiro script
-    print("Gerando Relatório de Cadastros Vinculados (SISAB EQUIPES HOMOLOGADAS)")
-    executar_equipes_homologadas(driver, pasta_download)
-
-    # Reconfigurar o driver para o segundo script
+    # Inicializar o driver
     driver = setup_driver()
 
-    # Executar o segundo script
-    print("Gerando Relatório de Cadastros Vinculados (SISAB TODAS EQUIPES)")
-    executar_todas_equipes(driver, pasta_download)
+    try:
+        # Executar o primeiro script
+        print("Gerando Relatório de Cadastros Vinculados (SISAB EQUIPES HOMOLOGADAS)")
+        executar_equipes_homologadas(driver, pasta_download)
+
+    finally:
+        # Fechar o driver após o primeiro script
+        driver.quit()
+
+    # Aguardar um pouco antes de reiniciar o driver
+    time.sleep(10)
+
+    # Inicializar o driver novamente
+    driver = setup_driver()
+
+    try:
+        # Executar o segundo script
+        print("Gerando Relatório de Cadastros Vinculados (SISAB TODAS EQUIPES)")
+        executar_todas_equipes(driver, pasta_download)
+
+    finally:
+        # Fechar o driver após o segundo script
+        driver.quit()
 
 if __name__ == "__main__":
     main()
